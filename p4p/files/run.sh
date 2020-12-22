905464935f35c46fd3cfffb2560bdb308a6d61aa
hexagrimoire <hexagrimoire@gmail.com>
2020-12-22 12:40:12 +0900

    first add commit

---
+++ p4p/files/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo -E -u perforce p4d -Gc
+sudo -E -u perforce p4d -Gf
+sudo -E -u perforce p4 trust -y
+sudo -E -u perforce p4p -d -p ${PORT} -t ${P4TARGET} -r ${P4PCACHE} -L ${P4PLOGFILE} -v 3
+
