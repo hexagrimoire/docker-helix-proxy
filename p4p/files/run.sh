fe84cbeaebcdb09ab6458ea4ed3be59d4bc8dbfe
hexagrimoire <hexagrimoire@gmail.com>
2021-02-02 06:23:02 +0900

    改行コードなどの修正

--- p4p/files/run.sh
+++ p4p/files/run.sh
@@ -3,5 +3,4 @@
 sudo -E -u perforce p4d -Gc
 sudo -E -u perforce p4d -Gf
 sudo -E -u perforce p4 trust -y
-sudo -E -u perforce p4p -d -p ${PORT} -t ${P4TARGET} -r ${P4PCACHE} -L ${P4PLOGFILE} -v 3
-
+sudo -E -u perforce p4p -d -p ${PORT} -t ${P4PORT} -r ${P4PCACHE} -L ${P4PLOGFILE} -v 3
